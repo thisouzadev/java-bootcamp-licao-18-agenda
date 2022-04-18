169027bb3f11ee5032a8b804722037f0d2c08b0b
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-04-18 08:25:47 -0300

    finish

--- src/main/java/com/trybe/acc/java/Agenda.java
+++ src/main/java/com/trybe/acc/java/Agenda.java
@@ -4,7 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Agenda {
-	private List<String> contatos = new ArrayList<>();
-	
-	
+  private List<String> contatos = new ArrayList<>();
+
+  public void incluirContato(String nome) {
+    contatos.add(nome);
+  }
+
+  public void excluirContato(String nome) {
+    contatos.remove(nome);
+  }
+
+  public Boolean verificaContato(String nome) {
+    return contatos.contains(nome);
+  }
+
+  public int tamanhoAgenda() {
+    return contatos.size();
+  }
 }
